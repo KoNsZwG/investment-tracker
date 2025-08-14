2247256f5232882e33c3f3af8133c79909f54cbb
Konstantinos Zografos <konszogr@hotmail.com>
2025-08-14 23:20:38 +0300

    firebase keys move to env

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDSRYpFBIsg8Bia-JLfNdHxR0z3_SjP1ic',
-  authDomain: 'investment-tracker-df708.firebaseapp.com',
-  projectId: 'investment-tracker-df708',
-  storageBucket: 'investment-tracker-df708.appspot.com',
-  messagingSenderId: '302387048039',
-  appId: '1:302387048039:web:5c9f11216eca1bd1956ec2',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
